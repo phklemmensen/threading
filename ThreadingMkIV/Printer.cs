2c93ef57779a9cd6cc266e86c2945d3802687cb4
phklemmensen <phklemmensen>
2015-04-10 14:38:25 +0200

    MkIV and MkV added

---
+++ ThreadingMkIV/Printer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadingMkIV
+{
+    class Printer
+    {
+        private Action<string> cwl = Console.WriteLine;
+        private Action<string> cw = Console.Write;
+
+        public void PrintNumbers()
+        {
+            cwl(string.Format("-> {0} is executing PrintNumbers()", Thread.CurrentThread.Name));
+
+            cw("Your numbers: ");
+            for (int i = 0; i < 10; i++)
+            {
+                cw(string.Format("{0}, ", i));
+                Thread.Sleep(2000);
+            }
+
+            cwl("");
+        }
+    }
+}
---
+++ ThreadingMkIV/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreadingMkIV
+{
+    class Program
+    {
+        private static Action<string> cwl = Console.WriteLine;
+        private static Action<string> cw = Console.Write;
+        private static Func<string> cr = Console.ReadLine; 
+
+        static void Main(string[] args)
+        {
+            cw("Choose \"1\" or \"2\". Normal or multithreaded.");
+
+            Thread.CurrentThread.Name = "Primary";
+
+            string path = cr();
+
+            cwl(string.Format("-> {0} is executing PrintNumbers()", Thread.CurrentThread.Name));
+
+            var printer = new Printer();
+            switch (path)
+            {
+                case "2":
+                    ThreadStart run = printer.PrintNumbers;
+                    Thread backgroundThread = new Thread(run);
+                    backgroundThread.Name = "Secondary";
+                    backgroundThread.Start();
+                    break;
+                case "1":
+                default:
+                    printer.PrintNumbers();
+                    break;
+            }
+
+            MessageBox.Show("Busy waiting!");
+        }
+    }
+}
